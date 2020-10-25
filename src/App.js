80a3ee0158042ac206abd24e307553ae8bf3d743
shankar p <shankar369>
2020-10-25 12:00:41 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Series Tracker title</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Series Tracker</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
